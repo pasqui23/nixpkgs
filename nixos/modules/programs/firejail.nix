81fc8b89e8ad0225e04335b410bc01ffd45a4ff0
Pasquale <p3dimaria@hotmail.it>
2019-06-28 18:02:33 +0200

    Added programs.firejail.wrappedPackages option

--- nixos/modules/programs/firejail.nix
+++ nixos/modules/programs/firejail.nix
@@ -5,20 +5,6 @@ with lib;
 let
   cfg = config.programs.firejail;
 
-  wrappedBins = pkgs.stdenv.mkDerivation rec {
-    name = "firejail-wrapped-binaries";
-    nativeBuildInputs = with pkgs; [ makeWrapper ];
-    buildCommand = ''
-      mkdir -p $out/bin
-      ${lib.concatStringsSep "\n" (lib.mapAttrsToList (command: binary: ''
-      cat <<_EOF >$out/bin/${command}
-      #!${pkgs.stdenv.shell} -e
-      /run/wrappers/bin/firejail ${binary} "\$@"
-      _EOF
-      chmod 0755 $out/bin/${command}
-      '') cfg.wrappedBinaries)}
-    '';
-  };
 
 in {
   options.programs.firejail = {
@@ -36,12 +22,49 @@ in {
         not wrapped if they specify the absolute path to the binary.
       '';
     };
+
+    wrappedPackages = mkOption {
+      type = with types; listOf package;
+      default = [];
+      description = ''
+        Packages which have their execution wrapped with firejail.
+      '';
+    };
   };
 
   config = mkIf cfg.enable {
     security.wrappers.firejail.source = "${lib.getBin pkgs.firejail}/bin/firejail";
 
-    environment.systemPackages = [ wrappedBins ];
+    environment.systemPackages =
+    let
+      wrappedBins = pkgs.stdenv.mkDerivation rec {
+        name = "firejail-wrapped-binaries";
+        nativeBuildInputs = with pkgs; [ makeWrapper ];
+        buildCommand = ''
+          mkdir -p $out/bin
+          ${lib.concatStringsSep "\n" (lib.mapAttrsToList (command: binary: ''
+          cat <<_EOF >$out/bin/${command}
+          #!${pkgs.stdenv.shell} -e
+          /run/wrappers/bin/firejail ${binary} "\$@"
+          _EOF
+          chmod 0755 $out/bin/${command}
+          '') cfg.wrappedBinaries)}
+        '';
+
+        wrappedPkgs = with pkgs; runCommand "firejail-wrap-packages" {} ''
+          mkdir -p $out/nix/store
+          ${string.concatMapStrings (p: ''
+            cp ${p} $out
+          '') cfg.wrappedPackages}
+          ln -s {,$out}${firejail}
+          chroot $out firecfg
+          rm $out${firejail}
+          rm -rf $out/nix/store
+        '';
+      };
+    in
+      optional ([]!=cfg.wrappedBinaries) wrappedBins ++
+      optional ([]!=cfg.wrappedPackages) wrappedPkgs;
   };
 
   meta.maintainers = with maintainers; [ peterhoeg ];
